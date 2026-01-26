189c0975fa7c0428afeed8cf00c7283e399027b6
eliezer-jimenez_claroca <eliezer.jimenez@claro.com.gt>
2026-01-26 14:43:51 -0600

    feat(component): :heavy_plus_sign: agrega componente geolocation

---
+++ src/app/core/components/parking-crud/geolocation/geolocation/geolocation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-geolocation',
+  standalone: true,
+  imports: [],
+  templateUrl: './geolocation.component.html',
+  styleUrl: './geolocation.component.scss'
+})
+export class GeolocationComponent {
+
+}
