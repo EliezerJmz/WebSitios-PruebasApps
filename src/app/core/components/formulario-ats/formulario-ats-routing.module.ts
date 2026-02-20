fbc9c37617368ff51e53f6ea02cefd4e60366fcc
eliezer-jimenez_claroca <eliezer.jimenez@claro.com.gt>
2026-02-20 11:18:20 -0600

    refactor(routing): :ambulance: ordena rutas del formulario ATS

--- src/app/core/components/formulario-ats/formulario-ats-routing.module.ts
+++ src/app/core/components/formulario-ats/formulario-ats-routing.module.ts
@@ -1,32 +1,31 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { FormularioAtsComponent } from './formulario-ats.component';
 import { ViewParkingComponent } from './view-parking/view-parking.component';
 import { AuthGuard } from '../../guards/auth.guard';
 
 
-
-@NgModule({
-	imports: [RouterModule.forChild([
-		{ path: '',component: FormularioAtsComponent},
-        {path: '',component: ViewParkingComponent},
-
+const routes: Routes = [
+        { path: '',component: FormularioAtsComponent},
+        { path: '',component: ViewParkingComponent},
         { path: 'create-parking',
-                        loadChildren: () => import('../create-parking/create-parking.module').then(m => m.CreateParkingModule),
-                        canActivate: [AuthGuard]
+            loadChildren: () => import('../create-parking/create-parking.module').then(m => m.CreateParkingModule),
+            canActivate: [AuthGuard]
         },
         { path: 'view-parking',
-        loadChildren: () => import('./view-parking/view-parking.module').then(m=> m.ViewParkingModule),
-        canActivate: [AuthGuard]
+            loadChildren: () => import('./view-parking/view-parking.module').then(m=> m.ViewParkingModule),
+            canActivate: [AuthGuard]
         },
         { path: 'edit-parking',
-        loadChildren: () => import('../edit-parking/emptydemo.module').then(m => m.EmptyDemoModule),
-        canActivate: [AuthGuard]
+            loadChildren: () => import('../edit-parking/emptydemo.module').then(m => m.EmptyDemoModule),
+            canActivate: [AuthGuard]
         },
+  
+];
 
 
-
-	])],
+@NgModule({
+	imports: [RouterModule.forChild(routes)],
 	exports: [RouterModule]
 })
 export class FormularioAtsRoutingModule { }
