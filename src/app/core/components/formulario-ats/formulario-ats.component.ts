137b16afdc3512bf730cdf3f55d4ba103a8c0f9a
eliezer-jimenez_claroca <eliezer.jimenez@claro.com.gt>
2026-04-09 12:29:37 -0600

    feat(component): :heavy_plus_sign: agrega formulario dinámico ATS permisos de trabajo trabajos con excavaciones

--- src/app/core/components/formulario-ats/formulario-ats.component.ts
+++ src/app/core/components/formulario-ats/formulario-ats.component.ts
@@ -264,6 +264,14 @@ obtenerFormulariosPublicados() {
       (field as any).subcategory === 'TRABAJOS_ESPACIOS_CONFINADOS'
     );
   }
+   getFilteredFieldsPermisosTrabajoExcavaciones() {
+    return this.fields.filter(field => 
+      (field as any).typeform === 'ANALISIS_TRABAJO_SEGURO' && 
+      (field as any).category === 'PERMISOS_TRABAJO' &&
+      (field as any).subcategory === 'TRABAJOS_EXCAVACIONES'
+    );
+  }
+
 
 
   onSubmit() {
