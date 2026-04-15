dbf475b402724fdde0fa0e4c526eaf4b124b9c57
eliezer-jimenez_claroca <eliezer.jimenez@claro.com.gt>
2026-04-15 12:32:58 -0600

    feat(service): :zap: agrega servicio para consumir end point respuestas de formularios ATS

--- src/app/core/service/formATS/form-ats.service.ts
+++ src/app/core/service/formATS/form-ats.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { FormATS } from '../../api/FormATS/formATS.model';
 import { Observable } from 'rxjs';
 import { ApiUrlService } from '../apiUrl/api-url.service';
+import { AnswersATS } from '../../api/FormATS/answersATS.model';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,18 @@ export class FormATSService {
 
   getFormularioATS(id: string): Observable<FormATS> {
     return this.http.get<FormATS>(`${this.API_URL}/${id}`);
+  }
+
+  submitFormAnswersATS(formularioId: string, answers: any, metadata: any, userId: string): Observable<AnswersATS> {
+    const URL = this.apiUrlService.apiSubmitAnswersATS(formularioId);
+    const payload = {
+      answers,
+      metadata: {
+        ...metadata,
+        usuarioId: userId
+      }
+    };
+    return this.http.post<AnswersATS>(URL, payload);
   }
 
 }
