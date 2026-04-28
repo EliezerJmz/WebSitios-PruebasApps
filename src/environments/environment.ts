83ee7007a5189f31ddff462ab12bad3e135d9c09
eliezer-jimenez_claroca <eliezer.jimenez@claro.com.gt>
2026-04-28 12:36:33 -0600

    refactor(environment): :fire: a localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,8 @@ export const environment = {
   //apiUrlBase:'https://api.devparqueosrrhh.claro.com.gt/',
   
   // URLs HTTP para desarrollo local (SOLO PARA DEBUG):
-  //apiUrlBase:'http://localhost:8080/', // Para emulador solamente
-  apiUrlBase:'http://10.204.227.179:8080/', // ipv4 local GIO
+  apiUrlBase:'http://localhost:8080/', // Para emulador solamente
+  //apiUrlBase:'http://10.204.227.179:8080/', // ipv4 local GIO
 
   apiVersion:'api/v1/',
   
